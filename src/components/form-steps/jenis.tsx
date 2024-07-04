4b4b47081e552934dc9a83d776d536ad3999522f
Naufal M <90486792+Mznaufal@users.noreply.github.com>
2024-07-05 05:41:42 +0700

    Update jenis.tsx

--- src/components/form-steps/jenis.tsx
+++ src/components/form-steps/jenis.tsx
@@ -6,33 +6,34 @@ import { motion } from "framer-motion";
 
 const options = [
   {
-    id: "mahasiswa",
-    label: "Mahasiswa",
-    imgSrc: "student.svg",
+    id: "traditional",
+    label: "Tradisional",
+    imgSrc: "traditional.svg",
+    occupations: ["mahasiswa", "pekerja-kantoran", "professional", "others"],
+    usage: ["daily-use", "multimedia", "gaming"],
   },
   {
-    id: "pekerja-kantoran",
-    label: "Pekerja Kantoran",
-    imgSrc: "office-worker.svg",
+    id: "2in1-laptop",
+    label: "2-in-1 Laptop",
+    imgSrc: "2in1.svg",
+    occupations: ["mahasiswa", "professional", "others"],
+    usage: ["multimedia"],
   },
   {
-    id: "professional",
-    label: "Professional",
-    imgSrc: "pro.svg",
-  },
-  {
-    id: "others",
-    label: "Others",
-    imgSrc: "others.svg",
+    id: "special-laptop",
+    label: "Special Laptop",
+    imgSrc: "special.svg",
+    occupations: ["pekerja-kantoran", "professional", "others"],
+    usage: ["multimedia", "office"],
   },
 ];
 
-export function Pekerjaan() {
+export function Jenis() {
   const data = useFormStore((state: any) => state.user);
   const updateUser = useFormStore((state: any) => state.updateUser);
 
-  function handleOccupationChange(id: string) {
-    updateUser({ pekerjaan: id });
+  function handleJenisChange(id: string) {
+    updateUser({ jenis_laptop: id });
   }
 
   return (
@@ -43,26 +44,41 @@ export function Pekerjaan() {
         exit={{ opacity: 0, x: 20 }}
       >
         <h1 className="text-left scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-          Pilih Pekerjaan
+          Pilih Jenis Laptop
         </h1>
         <h2 className="text-[#b8c0e0] text-left scroll-m-20 text-lg tracking-tight">
-          Pilih pekerjaan yang paling sesuai dengan anda
+          Pilih jenis laptop yang paling sesuai dengan penggunaan anda
         </h2>
       </motion.div>
       <motion.div
         initial={{ opacity: 0, x: 20 }}
         animate={{ opacity: 1, x: 0 }}
+        // transition={{ delay: 0.3 }}
         exit={{ opacity: 0, x: 20 }}
         className="flex md:flex-row flex-col gap-5"
       >
-        {options.map((option) => (
-          <RadioCard
-            key={option.id}
-            {...option}
-            selected={option.id === data.pekerjaan}
-            onChange={handleOccupationChange}
-          />
-        ))}
+        {options
+          .filter(
+            (option) =>
+              option.occupations.includes(data.pekerjaan) &&
+              option.usage.includes(data.penggunaan)
+          )
+          .map((option) => {
+            if (
+              data.pekerjaan === "professional" &&
+              data.penggunaan === "multimedia" &&
+              option.id === "traditional"
+            )
+              return;
+            return (
+              <RadioCard
+                key={option.id}
+                {...option}
+                selected={option.id === data.jenis_laptop}
+                onChange={handleJenisChange}
+              />
+            );
+          })}
       </motion.div>
     </div>
   );
