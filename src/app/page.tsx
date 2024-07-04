0c96db77c16bf50d0ae6e95bb49f8fedda70f051
Naufal M <90486792+Mznaufal@users.noreply.github.com>
2024-07-05 05:27:18 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,75 +1,81 @@
-import { useTheme } from './themeContext';
+// src/app/page.tsx
+"use client";
+
+import { useTheme, ThemeProvider } from './themeContext';
 import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   const { theme, toggleTheme } = useTheme();
+
   return (
-    <MaxWidthWrapper className="mb-12 mt-20 flex flex-col items-center justify-center text-center">
-      <div className="mb-10 flex flex-col gap-12 items-center">
-        <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight lg:text-6xl">
-          Temukan{" "}
-          <span className="text-transparent font-bold bg-clip-text bg-gradient-to-r from-[#b4befe] via-[#f5c2e7] to-[#fab387]">
-            Laptop
-          </span>{" "}
-          Ideal Anda
-        </h1>
-        <h2 className="text-xl flex flex-col gap-2 md:px-0 px-5">
-          <span>
-            Sumber terpercaya untuk rekomendasi laptop yang sesuai dengan
-            kebutuhan,
-          </span>
-          dari pelajar, pekerja kantoran, hingga gamer.
-        </h2>
-        <div className="flex-basis:auto w-fit">
-          <Link href="/find">
-            <p className="text-base rounded-full px-7 py-3 font-semibold shadow-glow bg-white text-black">
-              Cari Laptop
+    <ThemeProvider>
+      <MaxWidthWrapper className="mb-12 mt-20 flex flex-col items-center justify-center text-center">
+        <div className="mb-10 flex flex-col gap-12 items-center">
+          <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight lg:text-6xl">
+            Temukan{" "}
+            <span className="text-transparent font-bold bg-clip-text bg-gradient-to-r from-[#b4befe] via-[#f5c2e7] to-[#fab387]">
+              Laptop
+            </span>{" "}
+            Ideal Anda
+          </h1>
+          <h2 className="text-xl flex flex-col gap-2 md:px-0 px-5">
+            <span>
+              Sumber terpercaya untuk rekomendasi laptop yang sesuai dengan
+              kebutuhan,
+            </span>
+            dari pelajar, pekerja kantoran, hingga gamer.
+          </h2>
+          <div className="flex-basis:auto w-fit">
+            <Link href="/find">
+              <p className="text-base rounded-full px-7 py-3 font-semibold shadow-glow bg-white text-black">
+                Cari Laptop
+              </p>
+            </Link>
+          </div>
+        </div>
+        <Image
+          src="/test.svg"
+          alt="laptop"
+          className="md:px-0 px-10"
+          width={500}
+          height={500}
+        />
+        <section
+          id="about"
+          className="mt-20 flex flex-col justify-center items-center space-y-10 md:px-0 px-7"
+        >
+          <p className="scroll-m-20 pb-2 md:text-3xl text-xl font-semibold tracking-tight first:mt-0 text-left max-w-4xl leading-7 [&:not(:first-child)]:mt-6">
+            Finde hadir untuk menyederhanakan proses mencari laptop serta
+            memberikan panduan yang informatif
+          </p>
+          <div className="max-w-4xl text-left space-y-5">
+            <p className="leading-7 [&:not(:first-child)]:mt-6">
+              Kami berkomitmen untuk menyediakan informasi terkini dan
+              terkualifikasi tentang berbagai laptop di pasaran, membantu Anda
+              membuat keputusan yang cerdas saat mencari perangkat yang sesuai
+              dengan kebutuhan Anda.
             </p>
-          </Link>
-        </div>
-      </div>
-      <Image
-        src="/test.svg"
-        alt="laptop"
-        className="md:px-0 px-10"
-        width={500}
-        height={500}
-      />
-      <section
-        id="about"
-        className="mt-20 flex flex-col justify-center items-center space-y-10 md:px-0 px-7"
-      >
-        <p className="scroll-m-20 pb-2 md:text-3xl text-xl font-semibold tracking-tight first:mt-0 text-left max-w-4xl leading-7 [&:not(:first-child)]:mt-6">
-          Finde hadir untuk menyederhanakan proses mencari laptop serta
-          memberikan panduan yang informatif
-        </p>
-        <div className="max-w-4xl text-left space-y-5">
-          <p className="leading-7 [&:not(:first-child)]:mt-6">
-            Kami berkomitmen untuk menyediakan informasi terkini dan
-            terkualifikasi tentang berbagai laptop di pasaran, membantu Anda
-            membuat keputusan yang cerdas saat mencari perangkat yang sesuai
-            dengan kebutuhan Anda.
-          </p>
-          <p className="leading-7 [&:not(:first-child)]:mt-6">
-            Misi utama kami adalah membantu pengguna seperti Anda menemukan
-            laptop yang tepat, yang memenuhi kebutuhan sehari-hari maupun
-            kebutuhan khusus seperti produktivitas, gaming, desain grafis, dan
-            banyak lagi. Kami mengerti bahwa memilih laptop dapat menjadi tugas
-            yang rumit, mengingat variasi luas dalam spesifikasi, merek, dan
-            model yang ada.
-          </p>
-        </div>
-      </section>
-      {/* <Image src="/laptop-3.png" alt="laptop" width={1370} height={743} /> */}
-      {/* <Model /> */}
-      <button
-        className='mt-10 px-4 py-2 bg-gray-800 text-white rounded'
-        onClick={toggleTheme}
-      >
-        Toggle Theme
-      </button>
-    </MaxWidthWrapper>
+            <p className="leading-7 [&:not(:first-child)]:mt-6">
+              Misi utama kami adalah membantu pengguna seperti Anda menemukan
+              laptop yang tepat, yang memenuhi kebutuhan sehari-hari maupun
+              kebutuhan khusus seperti produktivitas, gaming, desain grafis, dan
+              banyak lagi. Kami mengerti bahwa memilih laptop dapat menjadi tugas
+              yang rumit, mengingat variasi luas dalam spesifikasi, merek, dan
+              model yang ada.
+            </p>
+          </div>
+        </section>
+        {/* <Image src="/laptop-3.png" alt="laptop" width={1370} height={743} /> */}
+        {/* <Model /> */}
+        <button
+          className='mt-10 px-4 py-2 bg-gray-800 text-white rounded'
+          onClick={toggleTheme}
+        >
+          Toggle Theme
+        </button>
+      </MaxWidthWrapper>
+    </ThemeProvider>
   );
 }
